61eacb0556bd7dba7c48294dc971dbffd8f3caa0
rohini <rohini@gmail.com>
2025-11-17 12:22:04 +0530

    py assesment

---
+++ python_assessment.py
@@ -0,0 +1,77 @@
+# Task: Complete the missing conditions to categorize age.
+
+def categorize_age(age):
+    # Fill the missing conditions
+    if :
+        return "Child"
+    elif  :
+        return "Teenager"
+    elif :
+        return "Adult"
+    else:
+        return "Senior"
+
+
+# Task: Complete the function so it returns the sum of all positive numbers in a list.
+def sum_positive(nums):
+    total = 0
+    # Fill in the loop and condition
+    for  :
+        if  :
+            
+    return total
+
+def count_words(words):
+    counts = {}     # dictionary to store word → frequency
+
+    # TODO: If the word is not in counts, initialize it.
+    # TODO: Otherwise, increase its count.
+   
+    return counts
+
+
+# Test the function count_words
+sample = ["apple", "banana", "apple", "orange", "banana", "apple"]
+
+print(count_words(sample))
+# Expected output (order may vary):
+# {'apple': 3, 'banana': 2, 'orange': 1}
+
+
+# Task: Fix the missing parts to avoid errors.
+
+def divide(a, b):
+    try:
+        result =       # perform division
+        return result
+    except  :          # fill error type
+        return "Cannot divide by zero"
+    
+
+
+class Rectangle:
+    def __init__(self):
+        self. = width
+        self. = height
+
+    def area(self):
+        # return area
+        return 
+
+    def perimeter(self):
+        # return perimeter
+        return 
+
+
+class :
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return      # base behavior
+
+class Dog():
+    def speak(self):
+        # override behavior
+        return 
+
